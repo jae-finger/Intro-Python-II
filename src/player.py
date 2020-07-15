48a8b48bd678db7379d7f17ce27175431160604f
Jonathan Finger <jaefinger@gmail.com>
2020-07-15 11:20:53 -0500

    player class created

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player():
+    def __init__(self, room='outside'):
+        self.room = room
+    
+    def __str__(self):
+        return f'The player is currently located: {self.room}'
+
+jon = Player()
+print(jon)
